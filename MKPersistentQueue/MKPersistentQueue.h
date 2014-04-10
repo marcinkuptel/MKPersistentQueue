432b75fc3fe6fe574b2dababf762e57f12d01413
Marcin Kuptel <marcin@swiftkey.net>
2014-04-10 23:33:01 +0100

    Initial Commit

---
+++ MKPersistentQueue/MKPersistentQueue.h
@@ -0,0 +1,13 @@
+//
+//  MKPersistentQueue.h
+//  MKPersistentQueue
+//
+//  Created by Marcin Kuptel on 10/04/2014.
+//  Copyright (c) 2014 Marcin Kuptel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKPersistentQueue : NSObject
+
+@end
---
+++ MKPersistentQueueTests/MKPersistentQueueTests.m
@@ -0,0 +1,34 @@
+//
+//  MKPersistentQueueTests.m
+//  MKPersistentQueueTests
+//
+//  Created by Marcin Kuptel on 10/04/2014.
+//  Copyright (c) 2014 Marcin Kuptel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MKPersistentQueueTests : XCTestCase
+
+@end
+
+@implementation MKPersistentQueueTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
