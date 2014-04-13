b80ebc0858e27f38109ad05a407c379c3c640137
Marcin Kuptel <marcin@swiftkey.net>
2014-04-13 12:43:27 +0100

    Added a project for testing MKPersistentQueue

---
+++ MKPersistentQueueClient/MKPersistentQueueClient/MKViewController.h
@@ -0,0 +1,13 @@
+//
+//  MKViewController.h
+//  MKPersistentQueueClient
+//
+//  Created by Marcin Kuptel on 13/04/2014.
+//  Copyright (c) 2014 marcinkuptel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKViewController : UIViewController
+
+@end
---
+++ MKPersistentQueueClient/MKPersistentQueueClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MKPersistentQueueClient
+//
+//  Created by Marcin Kuptel on 13/04/2014.
+//  Copyright (c) 2014 marcinkuptel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MKAppDelegate class]));
+    }
+}
